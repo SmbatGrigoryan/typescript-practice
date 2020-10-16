6d010461c8ab5f78ebb776ffe01125f561ffe044
SmbatGrigoryan <smbatgrigoryan@gmail.com>
2020-10-16 18:25:22 +0400

    namespace code splitting

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,12 @@
+namespace App {
+  export   function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        return descriptor.value.bind(this);
+      }
+    };
+    return adjDescriptor;
+  }
+
+}
